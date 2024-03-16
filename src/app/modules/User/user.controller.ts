639f909d978e045b72a41a3919e6ceaa21070ebc
Emon <emonhasan7650@gmail.com>
2024-03-17 00:15:09 +0600

    get admin route and search functionality added

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,10 +2,22 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
+   try {
     const admin = req.body;
     
     const result = await userService.createAdmin(admin);
-    res.send(result);
+    res.status(200).json({
+        success: true,
+        message: "Admin created successfully!",
+        data: result
+    });
+   } catch (error: any) {
+    res.status(500).json({
+        success: false,
+        message: error?.message || "Error creating admin",
+        error: error
+    })
+   }
 }
 
 export const userController = {
