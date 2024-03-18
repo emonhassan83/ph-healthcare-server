75ead71b6cf13f51b35404fee42f423b0a50ba73
Emon <emonhasan7650@gmail.com>
2024-03-19 00:07:19 +0600

    admin apis created

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -13,6 +13,84 @@ const getAllFromDB = async (req: Request, res: Response) => {
     res.status(200).json({
       success: true,
       message: "Admin data fetched!",
+      meta: result.meta,
+      data: result.data,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
+};
+
+const getByIdFromDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await AdminService.getByIdFromDB(id);
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched by id!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
+};
+
+
+const updateIntoDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await AdminService.updateIntoDB(id, req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin data updated!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
+};
+
+const deleteFromDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await AdminService.deleteFromDB(id);
+    res.status(200).json({
+      success: true,
+      message: "Admin data deleted!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
+};
+
+const softDeleteFromDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await AdminService.softDeleteFromDB(id);
+    res.status(200).json({
+      success: true,
+      message: "Admin data deleted!",
       data: result,
     });
   } catch (err: any) {
@@ -26,4 +104,8 @@ const getAllFromDB = async (req: Request, res: Response) => {
 
 export const AdminController = {
   getAllFromDB,
+  getByIdFromDB,
+  updateIntoDB,
+  deleteFromDB,
+  softDeleteFromDB,
 };
